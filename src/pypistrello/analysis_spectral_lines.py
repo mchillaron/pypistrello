dffbce28e8d79ad5d21540df5098b5ff852b32c1
Maria Chillaron <mchillaron@users.noreply.github.com>
2026-01-15 22:04:15 +0100

    More arguments added to main

--- src/pypistrello/analysis_spectral_lines.py
+++ src/pypistrello/analysis_spectral_lines.py
@@ -13,19 +13,30 @@ import argparse
 import os
 import re
 
-def analysis_spectral_lines(fits_path, output_dir_path):
+def analysis_spectral_lines(fits_path, 
+                            output_dir_path, 
+                            wavelength_path, 
+                            config_path, 
+                            redshift, 
+                            line_restframe):
     """Main function to analyze spectral lines from a FITS file and save results to an output directory.
     
     Parameters
     ----------
     fits_path : Path
-        Path to the input FITS file containing spectral data.
+        Path to the input FITS file containing a table with coordinates and spectra.
     output_dir_path : Path
         Path to the output directory where results will be saved.
+    wavelength_path : Path
+        Path to the file containing the wavelength range to analyze.
+    config_path : Path
+        Path to the configuration YAML file with parameters for analysis.
+    redshift : float
+        Redshift value to adjust spectral lines.
+    line_restframe : list of float
+        List of rest-frame wavelengths of spectral lines to analyze.
     """
-
-    # Once we have the spectra in a FITS file and the directory in which we will be saving the results,
-    # we can proceed to read the FITS file to extract the spectra.
+ 
     pass
 
 
@@ -33,32 +44,38 @@ def analysis_spectral_lines(fits_path, output_dir_path):
 
 def main():
     parser = argparse.ArgumentParser(description='Python Package for Integrating Spectral lines using Trapezoids, Error estimation and Line-features Optimization.')
-    parser.add_argument('-F', '--input-file', type=str, required=True, help='Input file name containing spectral data, has to be FIT/FITS format')
+    parser.add_argument('-F', '--input-file', type=str, required=True, help='Input file name containing coordinates and spectra, has to be FIT/FITS table format')
+    parser.add_argument('-w', '--wavelength-range', type=str, required=True, help='Wavelength range to analyze, format: CSV')
+    parser.add_argument('-c', '--config-file', type=str, required=True, help='Configuration YAML filename with parameters for analysis')
     parser.add_argument('-o', '--output-dir', type=str, required=True, help='Output directory to save results')
+    parser.add_argument('-z', '--redshift', type=float, default=0.0, help='Redshift value to adjust spectral lines (default: 0.0)')
+    parser.add_argument('-lrf', '--line-restframe', type=float, nargs='+', required=True, help='Rest-frame wavelength of spectral line to analyze')
     args = parser.parse_args()
 
-    fits_name = args.input_file
+    fits_filename = args.input_file
+    wavelength_filename = args.wavelength_range
+    config_filename = args.config_file
     output_dir = args.output_dir
-
+    redshift = args.redshift
+    line_restframe = args.line_restframe
+    
     # INPUT PROTECTIONS
     # protection against non-FITS files
-    if not re.search(r'\.fits?$', fits_name, re.IGNORECASE):
-        raise ValueError(f"Input file '{fits_name}' is not a FIT/FITS file. Please provide a valid FIT/FITS file.")
+    if not re.search(r'\.fits?$', fits_filename, re.IGNORECASE):
+        raise ValueError(f"Input file '{fits_filename}' is not a FIT/FITS file. Please provide a valid FIT/FITS file.")
     
-    fits_path = Path('.')/fits_name
+    fits_path = Path('.')/fits_filename
     print(f"Input FITS file: {fits_path}")
     # make sure the input file exists
     if not fits_path.is_file():
-        raise FileNotFoundError(f"Input file '{fits_name}' does not exist. Please provide a valid file path.")
-    
-    
+        raise FileNotFoundError(f"Input file '{fits_filename}' does not exist. Please provide a valid file path.")
     
     # OUTPUT PROTECTIONS
     # protection against overwriting existing output directory
     if os.path.exists(output_dir):
         raise FileExistsError(f"Output directory '{output_dir}' already exists. Please choose a different name to avoid overwriting existing data.")
     
-    # make sure the output does not contain extensions becuase it is a directory
+    # make sure the output does not contain extensions because it is a directory
     if re.search(r'\.[a-zA-Z0-9]+$', output_dir): # this means there is a file extension: a "." followed by alphanumeric characters at the end of the string
         raise ValueError(f"Output directory '{output_dir}' should not contain file extensions.")
 
@@ -66,7 +83,13 @@ def main():
     output_dir_path.mkdir(parents=True, exist_ok=True)
     print(f"Output files will be saved at: {output_dir_path}")
 
-    analysis_spectral_lines(fits_path, output_dir_path)
+    wavelength_path = Path('.')/wavelength_filename
+    print(f"Wavelength range file: {wavelength_path}")
+
+    config_path = Path('.')/config_filename
+    print(f"Configuration file: {config_path}")
+
+    analysis_spectral_lines(fits_path, output_dir_path, wavelength_path, config_path, redshift, line_restframe)
 
 
 if __name__ == "__main__":
